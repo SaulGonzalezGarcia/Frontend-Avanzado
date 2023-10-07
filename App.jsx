971fa5117d67e35392c1cd5efe484cfa8a5844a7
Saul <127172165+SaulGonzalezGarcia@users.noreply.github.com>
2023-10-07 16:07:33 -0600

    React Forms
    First commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ReactHookForm from './components/ReactHookForm/ReactHookForm.jsx'
+import SimpleForm from './components/SimpleForm/SimpleForm.jsx'
+
+function App() {
+
+  return (
+    <>
+    <ReactHookForm />
+    <SimpleForm />
+    </>
+  )
+}
+
+export default App
